d165d2aad73cef54afc6e104bae667d442c605eb
Robert J Spencer <rj@game.net.au>
2023-07-31 18:50:32 +0800

    First pass at a QT reimplementation

--- tray_linux.c
+++ tray_linux.c
@@ -1,82 +1,90 @@
+#include <QApplication>
+#include <QSystemTrayIcon>
+#include <QMenu>
+#include <QAction>
+#include <QEventLoop>
+#include <QTimer>
 #include "tray.h"
-#include <string.h>
-#include <stddef.h>
-#include <libayatana-appindicator/app-indicator.h>
 
-#define TRAY_APPINDICATOR_ID "tray-id"
+static QApplication *app = nullptr;
+static QSystemTrayIcon *trayIcon = nullptr;
+static tray *tray = nullptr;
+static bool shouldExit = false;
 
-static struct tray *tray_instance;
-static AppIndicator *indicator = NULL;
-static bool continue_running = true;
+static void updateMenu(tray_menu_item *menu, QMenu *qmenu) {
+    for (; menu != NULL && menu->text != NULL; menu++) {
+        if (strcmp(menu->text, "-") == 0) {
+            qmenu->addSeparator();
+        } else {
+            QAction *action = qmenu->addAction(menu->text);
+            action->setDisabled(menu->disabled);
+            action->setCheckable(true);
+            action->setChecked(menu->checked);
+            QObject::connect(action, &QAction::triggered, [=]() { if (menu->cb) menu->cb(menu); });
 
-static void _tray_cb(GtkMenuItem *item, gpointer data) {
-    (void)item;
-    struct tray_menu_item *m = (struct tray_menu_item *)data;
-    m->cb(m);
-}
-
-static void _tray_menu_item_cb(GtkMenuItem *item, gpointer data) {
-  (void)item;
-  struct tray_menu_item *m = (struct tray_menu_item *)data;
-  m->cb(m);
-}
-
-static GtkMenuShell *_tray_menu_item(struct tray_menu_item *m) {
-  GtkMenuShell *menu = (GtkMenuShell *)gtk_menu_new();
-  for (; m != NULL && m->text != NULL; m++) {
-    GtkWidget *item;
-    if (strcmp(m->text, "-") == 0) {
-      item = gtk_separator_menu_item_new();
-    } else {
-      if (m->submenu != NULL) {
-        item = gtk_menu_item_new_with_label(m->text);
-        gtk_menu_item_set_submenu(GTK_MENU_ITEM(item),
-                                  GTK_WIDGET(_tray_menu_item(m->submenu)));
-      } else {
-        item = gtk_check_menu_item_new_with_label(m->text);
-        gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(item), !!m->checked);
-      }
-      gtk_widget_set_sensitive(item, !m->disabled);
-      if (m->cb != NULL) {
-        g_signal_connect(item, "activate", G_CALLBACK(_tray_menu_item_cb), m);
-      }
+            if (menu->submenu != NULL) {
+                QMenu *submenu = new QMenu;
+                action->setMenu(submenu);
+                updateMenu(menu->submenu, submenu);
+            }
+        }
     }
-    gtk_widget_show(item);
-    gtk_menu_shell_append(menu, item);
-  }
-  return menu;
 }
 
+extern "C" {
+
+TRAY_EXPORT
 struct tray * tray_get_instance() {
-  return tray_instance;
+    return tray;
 }
 
-bool tray_init(struct tray *tray) {
-  if (gtk_init_check(0, NULL) == FALSE) {
-    return false;
-  }
-  indicator = app_indicator_new(TRAY_APPINDICATOR_ID, tray->icon_filepath,
-                                APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
-  app_indicator_set_status(indicator, APP_INDICATOR_STATUS_ACTIVE);
-  tray_update(tray);
-  return true;
+TRAY_EXPORT
+int tray_init(struct tray *t) {
+    int argc = 0;
+    char *argv[] = { nullptr };
+
+    app = new QApplication(argc, argv);
+    trayIcon = new QSystemTrayIcon;
+    tray = t;
+
+    trayIcon->setIcon(QIcon(tray->icon_filepath));
+    QObject::connect(trayIcon, &QSystemTrayIcon::activated, [=](QSystemTrayIcon::ActivationReason reason) {
+        if (reason == QSystemTrayIcon::Trigger) {
+            if (tray->cb)
+                tray->cb(tray);
+        }
+    });
+
+    tray_update(tray);
+
+    return 0;
 }
 
-bool tray_loop(bool blocking) {
-  gtk_main_iteration_do(blocking);
-  return continue_running;
+TRAY_EXPORT
+int tray_loop(int blocking) {
+    QEventLoop loop;
+    QTimer::singleShot(blocking ? -1 : 0, &loop, &QEventLoop::quit);
+    loop.exec();
+
+    if (shouldExit)
+        return -1;
+
+    return 0;
 }
 
-void tray_update(struct tray *tray) {
-    struct _GtkMenu *menu = GTK_MENU(_tray_menu_item(tray->menu));
-  app_indicator_set_menu(indicator, GTK_MENU(_tray_menu_item(tray->menu)));
-  app_indicator_set_icon(indicator, tray->icon_filepath);
-  // GTK is all about reference counting, so previous menu should be destroyed
-  // here
-  //app_indicator_set_menu(indicator, GTK_MENU(_tray_menu_item(tray->menu)));
+TRAY_EXPORT
+void tray_update(struct tray *t) {
+    QMenu *menu = new QMenu;
+    updateMenu(t->menu, menu);
 
-  tray_instance = tray;
+    trayIcon->setContextMenu(menu);
+    trayIcon->setToolTip(t->tooltip);
+    trayIcon->show();
 }
 
-void tray_exit(void) { continue_running = false; }
+TRAY_EXPORT
+void tray_exit(void) {
+    shouldExit = true;
+}
 
+} // extern "C"
